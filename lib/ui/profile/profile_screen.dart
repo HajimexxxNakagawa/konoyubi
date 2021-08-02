8162abda7977f3350762bad97563a53db77918c8
Hajime Nakagawa <navyblue0914@icloud.com>
2021-08-02 17:26:21 +0900

    display user info

--- lib/ui/profile/profile_screen.dart
+++ lib/ui/profile/profile_screen.dart
@@ -1,12 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends HookWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    final snapshot = FirebaseFirestore.instance
+        .collection('userList')
+        .doc(user!.uid)
+        .snapshots();
+    final userInfo = useStream(snapshot);
+
+    if (!userInfo.hasData) {
+      return const ProfileErrorScreen();
+    } else {
+      final name = userInfo.data!['name'] as String;
+      return ProfileScreenVM(name: name);
+    }
+  }
+}
+
+class ProfileScreenVM extends StatelessWidget {
+  const ProfileScreenVM({Key? key, required this.name}) : super(key: key);
+
+  final String name;
+  @override
+  Widget build(BuildContext context) {
     return Center(
-      child: Text('Profile Screen'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Icon(Icons.person), Text(name)],
+      ),
+    );
+  }
+}
+
+class ProfileErrorScreen extends StatelessWidget {
+  const ProfileErrorScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Sorry, something went wrong...'),
     );
   }
 }
