86b09d9b686b7a778b2f9a0990f5bd3f4dc4037e
Hajime Nakagawa <navyblue0914@icloud.com>
2021-08-08 18:19:50 +0900

    add dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await dotenv.load(fileName: ".env");
   await SentryFlutter.init(
     (options) {
-      options.dsn =
-          'https://8ca720ad4ee548de839aebe3214c65da@o933562.ingest.sentry.io/5882678';
+      options.dsn = dotenv.env['SENTRY_DSN'];
     },
     appRunner: () => runApp(const ProviderScope(child: App())),
   );
