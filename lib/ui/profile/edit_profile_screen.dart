3daf0e149db97c84fbe4d4ae1afcaa0cc70fd6f9
Guchiwo0608 <guchiwo.cello.0608@gmail.com>
2021-08-14 15:03:04 +0900

    Add avater and biography

--- lib/ui/profile/edit_profile_screen.dart
+++ lib/ui/profile/edit_profile_screen.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:konoyubi/ui/components/typography.dart';
 import 'package:konoyubi/ui/theme/constants.dart';
 import 'package:konoyubi/ui/theme/height_width.dart';
 
@@ -10,6 +12,8 @@ final nameControllerProvider =
 final twitterControllerProvider =
     StateProvider<TextEditingController?>((ref) => TextEditingController());
 final facebookControllerProvider =
+    StateProvider<TextEditingController?>((ref) => TextEditingController());
+final biographyControllerProvider =
     StateProvider<TextEditingController?>((ref) => TextEditingController());
 
 class EditProfileScreen extends HookWidget {
@@ -21,6 +25,7 @@ class EditProfileScreen extends HookWidget {
     final nameController = useProvider(nameControllerProvider);
     final twitterController = useProvider(twitterControllerProvider);
     final facebookController = useProvider(facebookControllerProvider);
+    final biographyController = useProvider(biographyControllerProvider);
     useEffect(() {
       nameController.state?.text = "baka";
       return () {
@@ -40,7 +45,39 @@ class EditProfileScreen extends HookWidget {
           child: SizedBox(
             width: width * 0.9,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                const Spacer(flex: 2),
+                Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 150,
+                        child: CachedNetworkImage(
+                          imageUrl:
+                              "https://lh3.googleusercontent.com/a-/AOh14GiobA4jwQETrwF_K2bHqmQmdT9W9L2C7gtcBBivAA=s96-c",
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                        clipBehavior: Clip.antiAlias,
+                        decoration: const BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.grey),
+                      ),
+                      Align(
+                        child: Container(
+                          decoration: const BoxDecoration(),
+                          child: const Icon(Icons.camera_alt),
+                        ),
+                        alignment: Alignment.bottomLeft,
+                      )
+                    ],
+                  ),
+                ),
+                const Spacer(flex: 2),
                 ListTile(
                   leading: const Icon(
                     Icons.person,
@@ -74,6 +111,17 @@ class EditProfileScreen extends HookWidget {
                     controller: facebookController.state,
                   ),
                 ),
+                const Spacer(),
+                const Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Body1("Biography"),
+                ),
+                ProfileForm(
+                  hintText: "Tell us about you!",
+                  maxLines: 10,
+                  controller: biographyController.state,
+                ),
+                const Spacer(flex: 3),
               ],
             ),
           ),
@@ -84,16 +132,22 @@ class EditProfileScreen extends HookWidget {
 }
 
 class ProfileForm extends StatelessWidget {
-  const ProfileForm({Key? key, this.hintText, this.controller})
-      : super(key: key);
+  const ProfileForm({
+    Key? key,
+    this.hintText,
+    this.controller,
+    this.maxLines = 1,
+  }) : super(key: key);
 
   final String? hintText;
   final TextEditingController? controller;
+  final int maxLines;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
+      maxLines: maxLines,
       decoration: InputDecoration(
         hintText: hintText,
         focusedBorder: const OutlineInputBorder(
@@ -112,7 +166,7 @@ class ProfileForm extends StatelessWidget {
         ),
         fillColor: const Color(0xFFF7FAFC),
         filled: true,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 12),
+        contentPadding: const EdgeInsets.all(12),
       ),
       cursorColor: bodyColor,
     );
