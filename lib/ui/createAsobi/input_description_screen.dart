08d865f5f623226cc5a700500b80d667a1805dc0
Guchiwo0608 <guchiwo.cello.0608@gmail.com>
2021-08-17 18:12:33 +0900

    add asobiDescriptionValidation

--- lib/ui/createAsobi/input_description_screen.dart
+++ lib/ui/createAsobi/input_description_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:konoyubi/ui/createAsobi/create_asobi_screen_template.dart';
+import 'package:konoyubi/ui/utility/primary_dialog.dart';
 import 'package:konoyubi/ui/utility/transition.dart';
 import 'select_position_screen.dart';
 
@@ -14,20 +15,28 @@ class InputAsobiDescriptionScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final isAbsorb = useProvider(absorbStateProvider);
+    final asobiDescriptionController =
+        useProvider(asobiDescriptionControllerProvider);
     return AbsorbPointer(
       absorbing: isAbsorb.state,
       child: CreateAsobiScreenTemplate(
         title: 'セツメイを書く',
-        body: const Body(),
+        body: Body(
+          controller: asobiDescriptionController.state!,
+        ),
         index: 1,
         onBack: () {
           Navigator.pop(context);
         },
         onNext: () {
-          pageTransition(
-            context: context,
-            to: const SelectAsobiPositionScreen(),
-          );
+          if (asobiDescriptionValidation(
+              description: asobiDescriptionController.state!.text,
+              context: context)) {
+            pageTransition(
+              context: context,
+              to: const SelectAsobiPositionScreen(),
+            );
+          }
         },
       ),
     );
@@ -37,16 +46,39 @@ class InputAsobiDescriptionScreen extends HookWidget {
 class Body extends HookWidget {
   const Body({
     Key? key,
+    required this.controller,
   }) : super(key: key);
+
+  final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
-    final asobiDescriptionController =
-        useProvider(asobiDescriptionControllerProvider);
     return Center(
       child: TextField(
-        controller: asobiDescriptionController.state,
+        controller: controller,
       ),
     );
   }
+}
+
+bool asobiDescriptionValidation({
+  required String? description,
+  required BuildContext context,
+}) {
+  final isNotDescriptionEmpty = description != "";
+  final isDescriptionLengthNotOver = description!.length <= 12;
+  final isDescriptionContainsSpace =
+      description.contains(" ") || description.contains("　");
+  final isDescriptionNotOnlySpace = isDescriptionContainsSpace
+      ? isDescriptionContainsSpace && description.trim().isNotEmpty
+      : true;
+  if (!isNotDescriptionEmpty) {
+    showPrimaryDialog(context: context, content: "セツメイを入力してください");
+  }
+  if (!isDescriptionLengthNotOver) {
+    showPrimaryDialog(context: context, content: "20文字以内で！");
+  }
+  return isNotDescriptionEmpty &&
+      isDescriptionLengthNotOver &&
+      isDescriptionNotOnlySpace;
 }
